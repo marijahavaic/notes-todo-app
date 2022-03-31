205c1c1dc4a135e6027aaedd3aa69266386da0b7
Marija Havaic <marija.havaic@gmail.com>
2022-03-31 12:43:08 +0200

    Style Search component

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -2,11 +2,14 @@ import React from "react";
 
 import { MdSearch } from 'react-icons/md';
 
+import '../App.css';
+import '../Style/Note.css'
+
 const Search = () => {
     return (
         <div className="Search">
-            <MdSearch className="Icons" />
-            <input type="text" placeholdee="Type to search" />
+            <MdSearch className="Icon" />
+            <input type="text" placeholder="Type to search" />
         </div>)
 }
 
