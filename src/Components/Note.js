40570520e7ef7d0b5eb86095690d68ea965aa839
Marija Havaic <marija.havaic@gmail.com>
2022-04-06 14:10:50 +0200

    Add value and name for editNote

--- src/Components/Note.js
+++ src/Components/Note.js
@@ -3,17 +3,42 @@ import React, { useState } from 'react';
 import '../Style/Note.css'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare, faTrashCan, faClose } from '@fortawesome/free-solid-svg-icons';
 
-const Note = ({ id, title, text, handleDeleteNote }) => {
+const Note = ({ id, title, text, handleDeleteNote, handleAddNote, handleCloseNewNote }) => {
     const [isEditing, setIsEditing] = useState(false);
+
+
+    const [editTitle, setEditTitle] = useState(title);
+    const [editText, setEditText] = useState(text);
+
+    // set note title
+    const handleTitleEdit = (e) => {
+        setEditTitle(e.target.value)
+    }
+
+    // set note text
+    const handleTextEdit = (e) => {
+        setEditText(e.target.value);
+    }
+
+    // save note with calling addNote function from App.js
+    const handleSaveEditClick = () => {
+        // check if the note isn't empty
+        if (editText.trim().length > 0 || editTitle.trim().length > 0) {
+            handleAddNote(editTitle, editText);
+            // Clear input areas
+            setEditTitle('');
+            setEditText('');
+        }
+    }
 
     const toggleForm = () => {
         setIsEditing(!isEditing);
     }
 
 
-    return (
+    const viewNote = (
         <div className="Note" key={id}>
             <div className='NoteHeader'>
                 <h3>{title}</h3>
@@ -33,6 +58,44 @@ const Note = ({ id, title, text, handleDeleteNote }) => {
             <div className='NoteBody'>
                 <p>{text}</p>
             </div>
+        </div>
+    );
+
+    const editNote = (
+        <div className='NewNoteBg'>
+            <div className="Note NewNote">
+                <div className='NoteHeader'>
+                    <textarea
+                        rows={1}
+                        cols={28}
+                        placeholder="New Note"
+                        value={editTitle}
+                        name={title}
+                        onChange={handleTitleEdit}
+                    ></textarea>
+                    <FontAwesomeIcon icon={faClose} className="Icon" onClick={handleCloseNewNote} />
+                </div>
+                <div className='NoteBody'>
+                    <textarea
+                        rows={11}
+                        cols={31}
+                        placeholder="Type to add a note"
+                        value={editText}
+                        name={text}
+                        onChange={handleTextEdit}
+                    ></textarea>
+                </div>
+                <div className='NoteFooter'>
+                    <small>300 Remaining</small>
+                    <button className='save' onClick={handleSaveEditClick}>Save</button>
+                </div>
+            </div>
+        </div>
+    )
+
+    return (
+        <div>
+            {isEditing ? editNote : viewNote}
         </div>
     )
 }
